47cf2e9ddcea61014a3bb04ec4002d8dbab598a4
HaoQi <99021290+CWJSK@users.noreply.github.com>
2023-11-04 23:30:09 +0800

    Update jiuku.js

--- jiuku.js
+++ jiuku.js
@@ -1,1 +1,8 @@
 
+module.exports = {
+    platform: '九酷音乐', // 插件名
+    version: '0.0.0', // 版本号
+    srcUrl: 'https://raw.githubusercontent.com/CWJSK/tttttguan/main/jiuku.js', // 更新链接；当在app更新插件后会自动用此地址的插件覆盖
+    cacheControl: 'no-store', // 我们可以直接解析出musicItem的结构，因此选取no-store就好了，当然也可以不写这个字段
+}
+
